247fe450339b7835eae808486185afa16c4e2468
seunghyukshin <auseunghyuk@gmail.com>
2021-12-24 17:45:13 +0900

    updated 56 code

--- leetcode/56. Merge Intervals/56.js
+++ leetcode/56. Merge Intervals/56.js
@@ -6,7 +6,6 @@ const merge = function (intervals) {
   intervals.sort((a, b) => a[0] - b[0]);
 
   // 앞뒤 비교해서 b[0] <= a[1] 이면 a,b를 a[0] ~ b[1] 로 합쳐서 배열에 넣는다.
-  const length = intervals.length;
   for (let i = 1; i < intervals.length; i++) {
     if (intervals[i - 1][1] >= intervals[i][0]) {
       intervals.splice(i - 1, 2, [
